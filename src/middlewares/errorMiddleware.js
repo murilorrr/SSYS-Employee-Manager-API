ebb7e05fff432f79b8b51c8c80573b26a042649a
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-28 17:17:15 -0300

    feat: add a error middleware into your api

---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,13 @@
+const errorMiddleware = (err, _req, res, next) => {
+  if (err.code) {
+    return res.status(err.code).json(err);
+  }
+
+  if (err) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+
+  return next();
+};
+
+module.exports = errorMiddleware;
