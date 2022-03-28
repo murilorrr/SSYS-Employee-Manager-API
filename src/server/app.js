1db5fa1a87ec3e06934d53eafdef0e614713160a
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-28 17:18:05 -0300

    feat: use error middleware in app

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { StatusCodes } = require('http-status-codes');
+const errorMiddleware = require('../middlewares/errorMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.status(StatusCodes.OK).json({ message: 'OK' }));
+
+app.use(errorMiddleware);
 
 module.exports = app;
