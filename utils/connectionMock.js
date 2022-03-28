a2bf122018c554acb677bc5bcf09f75e4b1eede3
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-28 16:11:42 -0300

    build: setup with prettier and eslint airbnb

--- utils/connectionMock.js
+++ utils/connectionMock.js
@@ -5,7 +5,7 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const connection = async () => {
 
   const DBServer = await MongoMemoryServer.create();
-  const URLMock = await DBServer.getUri();
+  const URLMock = DBServer.getUri();
 
   return MongoClient.connect(URLMock, {
     useNewUrlParser: true,
