c76ba92ba2c279583b353bf1027c6e61e13cf8b7
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-29 01:36:59 -0300

    fix: prettier fix

--- src/database/seeders/20220328231103-employees.js
+++ src/database/seeders/20220328231103-employees.js
@@ -1,14 +1,10 @@
 'use strict';
 const { generateNEmployees } = require('../../../utils');
-const employees = generateNEmployees(Math.ceil(Math.random() * 99 ));
+const employees = generateNEmployees(Math.ceil(Math.random() * 99));
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert(
-      'Employees',
-      employees,
-      { timestamps: false }
-    );
+    await queryInterface.bulkInsert('Employees', employees, { timestamps: false });
   },
 
   down: async (queryInterface, Sequelize) => {
