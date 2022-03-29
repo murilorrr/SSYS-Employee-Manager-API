67ddc2e4d85c59406baa5dff5b3277085331ae12
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-28 21:59:14 -0300

    fix: database name start with uppercase

--- src/database/seeders/20220328231103-employees.js
+++ src/database/seeders/20220328231103-employees.js
@@ -10,7 +10,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      'employees',
+      'Employees',
       [
         {
           name: 'Anakin Skywalker',
