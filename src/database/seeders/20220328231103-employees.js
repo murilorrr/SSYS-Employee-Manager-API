230b4f7df717defd0970e7bf548a74ecdde10b09
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-29 14:43:10 -0300

    start database with faker employees

--- src/database/seeders/20220328231103-employees.js
+++ src/database/seeders/20220328231103-employees.js
@@ -1,10 +1,25 @@
 'use strict';
 const { generateNEmployees } = require('../../../utils');
+const { encryptPassword } = require('../../../utils')
 const employees = generateNEmployees(Math.ceil(Math.random() * 99));
+
+const defaultEmployee = {
+  name: 'Anakin Skywalker',
+  department: 'Architecture',
+  email: 'skywalker@ssys.com.br',
+  password: 'beStrong',
+  salary: '4000.00',
+  birth_date: '01-01-1983',
+};
+
+const seeders = [ defaultEmployee ,...employees ]
+seeders.forEach((employee) => {
+  employee.password = encryptPassword(employee.password);
+});
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Employees', employees, { timestamps: false });
+    await queryInterface.bulkInsert('Employees', seeders, { timestamps: false });
   },
 
   down: async (queryInterface, Sequelize) => {
