acbeee53df7d65442d55800b1afc946e856e2f12
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-29 21:21:32 -0300

    build: express-rate-limit add dependece to improve segurity

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const { StatusCodes } = require('http-status-codes');
 const errorMiddleware = require('../middlewares/errorMiddleware');
 const { employee, report } = require('../routes/index');
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 150, // Limit each IP to 150 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 app.use(bodyParser.json());
+app.use(limiter);
 
 app.get('/', (req, res) => res.status(StatusCodes.OK).json({ message: 'are you mad?' }));
 
